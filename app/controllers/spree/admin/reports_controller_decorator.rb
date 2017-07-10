6122fc6eb3dc6e60f7c06cbc0233c6a54c3a0df1
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2017-07-10 17:21:23 +0100

    Add reports controller decorator and add route to the new chart

---
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -0,0 +1,9 @@
+Spree::Admin::ReportsController.class_eval do
+  before_action :spree_variant_sales_reports_setup, only: :index
+
+  private
+
+  def spree_variant_sales_reports_setup
+    Spree::Admin::ReportsController.add_available_report!(:variant_sales_chart)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+  namespace :admin, path: Spree.admin_path do
+    resources :reports, only: [] do
+      collection do
+        get :variant_sales_chart, path: 'variant-sales-chart'
+      end
+    end
+  end
 end
